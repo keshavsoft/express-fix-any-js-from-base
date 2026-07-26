ab9a06287ed82111693694ffb250f5f3f44899d7
keshavsoft <nknnkumar@yahoo.com>
2026-07-26 17:34:46 +0530

    both good

--- test/forConsumption/v1/routes.js
+++ test/forConsumption/v1/routes.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 const router = express.Router();
 
+router.use('/v3', routerFromv3);
 router.use('/v4', routerFromv4);
-router.use('/v5', routerFromv5);
 
 export { router };
